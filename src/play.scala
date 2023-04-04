4afd5cb2dd0b9aae6624e4d99865fd2b7ac90542
Aayush141 <79538073+Aayush141@users.noreply.github.com>
2023-04-04 11:03:32 -0700

    Update play.scala

--- src/play.scala
+++ src/play.scala
@@ -22,13 +22,39 @@ def loop_each_word (sentence: => String) (body: => Unit) =
 
 //-------------------------------
 // unorderedString -- the characters in the string are not ordered, per say. 
+
 case class unorderedString(strt: String):
+
 	var strArr = (strt.toCharArray()).sorted
+	var strArrDist = strArr.distinct
+
 	override def toString = strArr.mkString("")
+
 	def ~~(newStr: unorderedString) = (strArr.sameElements(newStr.strArr))
+	def ~~=(newStr: unorderedString) = (strArrDist.sameElements(newStr.strArrDist))
+
+	def subsetOf(newStr: unorderedString) = (strArrDist.diff(newStr.strArrDist).isEmpty)
+
 
 given Conversion[String,unorderedString] = n => unorderedString(n)
+
+
+var g2 = "i am lord voldemort"
+var g3 = "i amd lordvv volodemort lloaer "
+var us1 = unorderedString("tom marvolo riddle ")
+var us2 = unorderedString("tik")
+
+us1 ~~ g2
+us1 ~~= g2
+us1 ~~ g3
+us1 ~~= g3
+
+us2.subsetOf("keith")
+us1.subsetOf(" qwertyuiopasdfghjklzxcvbnm")
+
 
 var g2 = "i am lord voldemort"
 var us1 = unorderedString("tom marvolo riddle ")
 us1 ~~ g2
+
+
