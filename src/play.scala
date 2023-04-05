93366c6dc78bc56ffce060e99d5d4d37f35bd438
Aayush141 <79538073+Aayush141@users.noreply.github.com>
2023-04-05 15:15:54 -0700

    Update play.scala

--- src/play.scala
+++ src/play.scala
@@ -1,6 +1,6 @@
 case class PhonemicString(strt: String):
 	var xSampa = List(List("@","ə"),List("E","ɛ")) 
-    // List(List("A", "ɑ"), List("a", "æ"), List("@", "ə"), List("E", "ɛ"), List("I", "ɪ"), List("i", "i"), List("O", "ɔ"), List("o", "o"), List("U", "ʊ"), List("u", "u"), List("3", "ɜ"), List("6", "ɐ"), List("7", "ɨ"), List("8", "ɵ"), List("9", "ɘ"), List("&", "æ"), List("@`", "ɚ"))
+    	// List(List("A", "ɑ"), List("@", "ə"), List("E", "ɛ"), List("ae", "æ"), List("I", "ɪ"), List("i", "i"), List("O", "ɔ"), List("o", "o"), List("U", "ʊ"), List("u", "u"), List("3", "ɜ"), List("6", "ɐ"), List("7", "ɨ"), List("8", "ɵ"), List("9", "ɘ"), List("&", "æ"), List("@`", "ɚ"))
 	def =~(newStr: PhonemicString) = (strt == newStr.strt)
 
 given Conversion[String,PhonemicString] = n => PhonemicString(n)
@@ -13,15 +13,20 @@ PhonemicString("he") =~ g
 
 
 //in progress loop strucure
-def loop_each_word (sentence: => String) (body: => Unit) =
+def loop_each_word (word: => String) (sentence: => String) (body: => Unit) =
 	val arr = sentence.split("[ -]")
 	for(word <- arr){
   		body
   	}
 
+loop_each_word (w) ("hE:lo-hE:loU-hE:loU") {
+	println(w)
+}
+
 
 //-------------------------------
 // unorderedString -- the characters in the string are not ordered, per say. 
+
 
 case class unorderedString(strt: String):
 
@@ -33,15 +38,17 @@ case class unorderedString(strt: String):
 	def ~~(newStr: unorderedString) = (strArr.sameElements(newStr.strArr))
 	def ~~=(newStr: unorderedString) = (strArrDist.sameElements(newStr.strArrDist))
 
-	def subsetOf(newStr: unorderedString) = (strArrDist.diff(newStr.strArrDist).isEmpty)
+	def ⊆(newStr: unorderedString) = (strArrDist.diff(newStr.strArrDist).isEmpty)
+	def ⊇(newStr: unorderedString) = ((newStr.strArrDist).diff(strArrDist).isEmpty)
 
 
 given Conversion[String,unorderedString] = n => unorderedString(n)
+given Conversion[unorderedString, String] = n => n.toString
 
 
 var g2 = "i am lord voldemort"
 var g3 = "i amd lordvv volodemort lloaer "
-var us1 = unorderedString("tom marvolo riddle ")
+var us1 = "tom marvolo riddle "
 var us2 = unorderedString("tik")
 
 us1 ~~ g2
@@ -49,8 +56,16 @@ us1 ~~= g2
 us1 ~~ g3
 us1 ~~= g3
 
-us2.subsetOf("keith")
-us1.subsetOf(" qwertyuiopasdfghjklzxcvbnm")
+"tik" ⊆ "keith"
+us1 ⊆ " qwertyuiopasdfghjklzxcvbnm"
+
+
+us2 = us1 + us2
+us2
+
+us1.length()
+
+
 
 
 var g2 = "i am lord voldemort"
