70984e9ae8b2d79dc9a9ea8ca18c36e750ebfcc3
Aayush141 <79538073+Aayush141@users.noreply.github.com>
2023-04-28 18:45:06 +0000

    sbt stuff aaaaa

---
+++ src/main/scala/test.scala
@@ -0,0 +1,76 @@
+import pppp._
+
+object TestApp {
+  @main
+  def main(args: Array[String]): Unit = {
+    "TEST STARTS HERE"↓
+
+    3 lines
+
+    ("/hE:l@/" =~ "/hɛ:lə/") -> ("case match"↓)
+
+    1 line
+
+    3 times {
+      (ana1(i) ~~ ana2) -> ("Anagram Found!"↓)
+    }
+
+    1 line
+
+    3 times {
+      (ana1(i) ⊆ ana2) -> ("Subset Found! "↓)
+    }
+
+    var tst = "hE:lə "/
+
+    1 line
+
+    tst↓
+
+    tst↓|
+
+    tst↓×
+
+    tst toIPACase
+
+    var phoneme = PhonemicString("heːtʃ tʃy@ʃt")
+    phoneme length
+
+    "lol kitty" ⊇ ("hello keith" - "hehe")
+
+    var ana1 = Array("i am lord voldemort", "emotio", "hullabaloo")
+    var ana2 = "tom marvolo riddle "
+    var uo1 = "tik"u
+
+    uo1 = ana2 + uo1
+    uo1
+    uo1 ? 'o'
+    uo1.length()
+
+    ()ln
+
+    false -> ("hullabaloo"↓)
+
+    1 line
+
+    var i = 0;
+    for_loop(10) {
+      println(i)
+    }
+
+    2 lines
+
+    for_loop(i < 10, i += 1) {
+      println(i * 2)
+    }
+
+    2 lines
+
+    for_loop((i = 0), i < 10, i += 1) {
+      println(i);
+    }
+
+    uo1 frequencyOf 'd'
+  }
+}
+
